5ed502ef5f00653680320f40d2eed51a832aafdf
shruti0802 <shru821999@gmail.com>
2021-10-08 23:24:31 +0530

    first commit

---
+++ test.sh
@@ -0,0 +1,20 @@
+
+#variable_name=variable_value
+
+NAME="Bridzelabz"
+echo $NAME
+
+#NAME="bnglr"
+readonly NAME
+
+age="23"
+#speak=True
+
+echo $NAME
+echo $age
+echo $speak
+
+names[0]="shruti"
+names[1]='betkar'
+echo ${names[*]}
+echo "First name: ${names[0]}"
